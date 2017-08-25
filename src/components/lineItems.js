dad9e639f0190105b1355978b8bec606396923f8
pmanukonda <padmasri_manukonda@intuit.com>
2017-08-24 19:03:44 -0700

    Added code to display the data to the console

--- src/components/lineItems.js
+++ src/components/lineItems.js
@@ -5,26 +5,38 @@ export default class LineItems extends React.Component {
 		super(props);
 		this.props = props;
 		this.state = {
-			amountFromLineItem: 0.0
+			amountFromLineItem: 0.0,
+			description: ""
 		}
 	}
 	render() {
 		return (
 			<div>
-				<input type="text" name="desc" className="descStyle"/>
-				<input type="text" name="amount" className="descStyle" onChange={this.onChange.bind(this)}/>
+				<input type="text" name="desc" onChange={this.onDescChange.bind(this)}/>
+				<input type="text" name="amount" onChange={this.onAmountChange.bind(this)}/>
 			</div>
 			);
 	}
 
-	onChange(event){
+	onAmountChange(event){
 		let amt = event.target.value;
+		let desc = this.state.description;
 		this.setState({
 				amountFromLineItem: amt
 		});
 		if(this.props.amountChangeListener) {
-				this.props.amountChangeListener(amt, this.props.id);
+				this.props.amountChangeListener(this.props.id, desc, amt);
+		}
+	}
+
+	onDescChange(event){
+		let desc = event.target.value;
+		let amt = this.state.amountFromLineItem;
+		this.setState({
+				description: desc
+		});
+		if(this.props.amountChangeListener) {
+				this.props.amountChangeListener(this.props.id, desc, amt);
 		}
 	}
 }
-
